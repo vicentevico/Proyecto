f70a05cdfd2fff14d1e1eaf65e9db8f381044792
Vicente <Vicente@192.168.120.187>
2017-04-20 18:58:42 +0200

    Aitor no está trabajando, ponme mas nota a mi Jorge :D

---
+++ src/modelo/Curs.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Vicente
+ */
+@Entity
+public class Curs implements Serializable{
+
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
